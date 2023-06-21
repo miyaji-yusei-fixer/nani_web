b8be9f4c676a9f8763e913067f2fd49570cb5e9c
miyaji-yusei-fixer <miyaji.yusei@fixer.co.jp>
2023-06-21 23:30:29 +0900

    :sparkles: na_ni

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,148 +2,43 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
+        <v-img :src="current" class="my-3" contain height="200" />
+      </v-col>
+
+      <v-row class="mb-4" justify="center" align-content="center">
         <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
+          class="ma-4"
+          v-for="(img, i) in gallery"
+          :key="`pic-${i}`"
+          max-height="100"
+          max-width="100"
+          :src="img.from"
+          @click="clickGallery(img)"
         />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
-        </h1>
-
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
+      </v-row>
     </v-row>
   </v-container>
 </template>
 
-<script>
+<script setup>
+import { ref } from "vue";
+const blank = require("../assets/blank.jpg")
+const na = require("../assets/na.jpg");
+const ni = require("../assets/ni.jpg");
+const na_se = new Audio(require("../assets/se/na.mp3"));
+const ni_se = new Audio(require("../assets/se/ni.mp3"));
 
-export default {
-  name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
-}
+const gallery = [
+  { from: na, se: na_se },
+  { from: ni, se: ni_se },
+];
+const current = ref(blank);
+const ring = (se) => {
+  se.currentTime = 0;
+  se.play();
+};
+const clickGallery = (img) => {
+  current.value = img.from;
+  ring(img.se);
+};
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './',
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
